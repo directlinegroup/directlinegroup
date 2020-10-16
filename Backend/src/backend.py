8c491926d723dbf8da576159c14b1c66c6c24f70
directlinegroup <andrey@vasilchenko.uk>
2020-10-16 20:32:50 +0100

    added timestamps

--- Backend/src/backend.py
+++ Backend/src/backend.py
@@ -1,12 +1,16 @@
 import requests
 import json
+from datetime import datetime
 
 
 def get_arg( numbers ):
     return { 'n' : numbers }
 
-def get_total_api( url = 'http://localhost:5000/total', numbers_to_add = []):
-    return requests.get(url, data=get_arg( numbers_to_add ))
+def get_total_api( url, numbers_to_add):
+    print("convert list to json ", datetime.now(tz=None))
+    data=get_arg( numbers_to_add )
+    print("call localhost api ", datetime.now(tz=None))
+    return requests.get(url, data=data)
 
 def get_sum_api( url = 'http://localhost:5000/total', n = 10000001):
   numbers_to_add = list(range(n))
@@ -15,4 +19,5 @@ def get_sum_api( url = 'http://localhost:5000/total', n = 10000001):
 if __name__ == '__main__':
   response = get_sum_api()
   print(response.text)
+  print("completed at ", datetime.now(tz=None))
 
